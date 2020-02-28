1c450c5dc0af76aabedff292f5075e565c1ede81
Yuriy <xett03@gmail.com>
2020-02-27 23:54:13 -0500

    1st try

---
+++ assets/java/game.js
@@ -0,0 +1,30 @@
+
+
+var questions = [
+    { q: "What pill took Neo?", a: "Red" },
+    { q: "Who gave Neo s red pill?", a: "Morpheus" },
+    { q: "Who was Neo's love?", a: "Trinity" },
+    { q: "Name a city where all people lived beside Matrix? ", a: "Zion" },
+    { q: "Name main Agent in Matrix", a: "Smith" }
+  ];
+  console.log(questions)
+
+const maxGuess = 3
+var pauseGame = false
+
+var guessedLetters = []
+var guessingWord = []
+var wordToMatch
+var numGuess
+var wins = 0
+
+document.onkeypress = function(event) {
+    // Make sure key pressed is an alpha character
+    if (isAlpha(event.key) && !pauseGame) {
+        checkForLetter(event.key.toUpperCase())
+    }
+}
+
+function isAlpha (ch){
+    return /^[A-Z]$/i.test(ch);
+}
